d6b237dc9374c8012bbd27eeeadcfe1b29f7fc80
Clesiaaa <robert.folga@outlook.com>
2026-02-14 20:06:20 +0100

    correction : compile.sh

--- compile.sh
+++ compile.sh
@@ -6,4 +6,4 @@ gcc -Wall -I./include -c src/algorithmes.c -o obj/alogorithmes.o
 
 gcc -Wall -I./include -c main.c -o obj/main.o
 
-gcc obj/main.o obj/graphe.o obj/noeud.o obj/alogorithmes.o -o out
+gcc obj/main.o obj/alogorithmes.o obj/graphe.o obj/noeud.o -o out
