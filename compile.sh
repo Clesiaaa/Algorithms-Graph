05add72cb7433dfdf0bbdf89e00becc42b17fc36
Clesiaaa <robert.folga@outlook.com>
2026-02-17 00:42:22 +0100

    Fixed : Compilation

--- compile.sh
+++
@@ -1,9 +0,0 @@
-gcc -Wall -I./include -c src/graphe.c -o obj/graphe.o
-
-gcc -Wall -I./include -c src/noeud.c -o obj/noeud.o
-
-gcc -Wall -I./include -c src/algorithmes.c -o obj/alogorithmes.o
-
-gcc -Wall -I./include -c main.c -o obj/main.o
-
-gcc obj/main.o obj/alogorithmes.o obj/graphe.o obj/noeud.o -o out
