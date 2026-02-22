bd2fc358d19f1a222fa66b6f881212314524bf7a
Clesiaaa <robert.folga@outlook.com>
2026-02-22 17:25:47 +0100

    add file

--- include/tools.h
+++ include/tools.h
@@ -8,6 +8,21 @@ typedef struct pile
     int *data;
 } pile;
 
+typedef struct file
+{   
+    int taille;
+    int *data;
+    int debut;
+    int fin;
+} file;
+
+file creer_file(int taille);
+void liberer_file(file *f);
+int file_vide(file *f);
+int file_pleine(file *f);
+void insere_file(file *f, int x);
+int supprime_file(file *f);
+
 pile creer_pile(int taille);
 void liberer_pile(pile *p);
 int depiler(pile *p);
--- src/tools.c
+++ src/tools.c
@@ -30,3 +30,41 @@ int pile_vide(pile *p) {
     if (p->sommet == 0) return 1;
     return 0;
 }
+
+file creer_file(int taille)
+{
+    file f;
+    f.taille = taille;
+    f.data = (int *)malloc(taille * sizeof(int));
+    f.debut = 0;
+    f.fin = 0;
+    return f;
+}
+
+void liberer_file(file *f) { free(f->data); }
+
+int file_vide(file *f) {
+    if (f->debut == f->fin) return 1;
+    return 0;
+}
+
+int file_pleine(file *f) {
+    if (f->debut == (f->fin + 1) % f->taille) return 1;
+    return 0;
+}
+
+void insere_file(file *f, int x) {
+    if (!file_pleine(f)){
+        f->data[f->fin] = x;
+        f->fin = (f->fin + 1) % f->taille;
+    }
+}
+
+int supprime_file(file *f) {
+    if (!file_vide(f)) {
+        int x = f->data[f->debut];
+        f->debut = (f->debut + 1) % f->taille;
+        return x;
+    }
+    return -1;
+}
